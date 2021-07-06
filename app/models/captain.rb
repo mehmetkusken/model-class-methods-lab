a1975209863a5c6643e4b313835b24d066fcb9d3
Mehmet <mehmetkusken@gmail.com>
2021-07-06 00:00:34 -0600

    commit tree d36c96ef7ff8b468fc02c45ae6821cb11967f6f8

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,24 +1,31 @@
 class Captain < ActiveRecord::Base
   has_many :boats
 
-  def self.catamaran_operators
-    # includes(boats: :classifications).where(classifications: {name: "Catamaran"})
+ def self.catamaran_operators
+    includes(boats: :classifications).where(classifications: {name: "Catamaran"})
+ end
+
+ def self.sailors
+   includes(boats: :classifications).where(classifications: {name: "Sailboat"}).uniq
+ end  
+
+  def self.motorboaters
+    includes(boats: :classifications).where(classifications: {name: "Motorboat"})
   end
 
-  def self.sailors
-    # includes(boats: :classifications).where(classifications: {name: "Sailboat"}).distinct
-  end
+ def self.non_sailors
+    includes(boats: :classifications).where.not(classifications: {name: "Sailboat"}).uniq
+ end
 
-  def self.motorboat_operators
-    # includes(boats: :classifications).where(classifications: {name: "Motorboat"})
-  end
-
-  def self.talented_seafarers
-    # where("id IN (?)", self.sailors.pluck(:id) & self.motorboat_operators.pluck(:id))
-  end
+ def self.talented_seafarers
+  where('id IN (?)', self.sailors.pluck(:id)).where('id IN (?)', self.motorboaters.pluck(:id))
+  #returns captains of motorboats and sailboats
+end
 
   def self.non_sailors
-    # where.not("id IN (?)", self.sailors.pluck(:id))
+    where.not("id IN(?)", self.sailors.pluck(:id))
   end
+
+
 
 end
